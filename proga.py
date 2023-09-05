2ec4626e001dfc6acd37c28777f4798d9e2cbc0e
IdrisLotus <144109783+IdrisLotus@users.noreply.github.com>
2023-09-05 18:56:08 +0930

    Add files via upload

---
+++ proga.py
@@ -0,0 +1,10 @@
+import time
+start_time= time.time()
+def fun():
+	a = 54654
+	b = 5454546
+	c = a + b
+end_time= time.time()
+fun()
+timetaker = end_time - start_time
+print("Your program takes:", timetaker)
